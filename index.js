39dfd8bfc5684e06daa7982a9b920cf434b52c61
Kevin Martensson <kevinmartensson@gmail.com>
2015-05-24 00:15:04 +0200

    Use lowercase header keys

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var got = require('got');
 
 module.exports = function (buf, cb) {
 	got.post('https://api.imgur.com/3/image', {
-		headers: {Authorization: 'Client-ID 34b90e75ab1c04b'},
+		headers: {authorization: 'Client-ID 34b90e75ab1c04b'},
 		body: buf
 	}, function (err, res) {
 		if (err) {
