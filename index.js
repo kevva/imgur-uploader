3aaaf8c89777a1c6c46f8a79f14297217dc7982d
Kevin Martensson <kevinmartensson@gmail.com>
2015-05-24 19:28:52 +0200

    Add streaming interface

--- index.js
+++ index.js
@@ -21,3 +21,29 @@ module.exports = function (buf, cb) {
 		cb(null, res);
 	});
 };
+
+module.exports.stream = function () {
+	var len = 0;
+	var res = [];
+	var stream = got.post('https://api.imgur.com/3/image', {
+		headers: {authorization: 'Client-ID 34b90e75ab1c04b'}
+	});
+
+	stream.on('data', function (data) {
+		res.push(data);
+		len += data.length;
+	});
+
+	stream.on('end', function () {
+		res = Buffer.concat(res, len).toString();
+		res = JSON.parse(res);
+		res = condenseKeys(res.data);
+		res.date = new Date(res.datetime * 1000);
+
+		delete res.datetime;
+
+		stream.emit('upload', res);
+	});
+
+	return stream;
+}
